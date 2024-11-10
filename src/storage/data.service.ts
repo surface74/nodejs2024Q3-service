68d709337538e647af2ae12ecc7ba1679ee91805
surface74 <surface.msn@gmail.com>
2024-11-10 13:02:16 +0300

    feat: implement filling users with fake data

--- src/storage/data.service.ts
+++ src/storage/data.service.ts
@@ -1,4 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
 import { User } from 'src/user/entities/user.entity';
+import * as users from './fake-data/users.json';
 
 class DataService {
   static instance: DataService = new DataService();
@@ -7,10 +9,27 @@ class DataService {
 
   private constructor() {
     if (!DataService.instance) DataService.instance = this;
+
+    this.fillUsers();
   }
 
   public static getInstance(): DataService {
     return DataService.instance;
+  }
+
+  private fillUsers(): void {
+    users.forEach((user: Pick<User, 'login' | 'password'>) => {
+      const { login, password } = user;
+      const newUser = {
+        id: uuidv4(),
+        login,
+        password,
+        version: 0,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      };
+      this.userStorage.push(newUser);
+    });
   }
 }
 
