d494037e8a24da9d3c647e6e09d21beb5cf01e65
surface74 <surface.msn@gmail.com>
2024-11-19 13:55:26 +0300

    refactor: refine using .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,10 @@ import { Favorite } from './favorites/entities/favorite.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      isGlobal: true,
+    }),
     DataModule,
     UserModule,
     ArtistModule,
@@ -26,8 +29,8 @@ import { Favorite } from './favorites/entities/favorite.entity';
     FavoritesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres_container',
-      port: +(process.env.POSTGRES_PORT || '5432'),
+      host: process.env.POSTGRES_DOMAIN || 'localhost',
+      port: +(process.env.POSTGRES_PORT || '5433'),
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'postgres',
       database: process.env.POSTGRES_DB || 'postgres',
